38981fe1e23e135ad8c55db33aee4b9d2cf1ad0a
Daniel Parsons <dpars@hotmail.co.uk>
2021-05-21 22:51:00 +0900

    Link change

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -22,7 +22,7 @@ function Welcome() {
                 <h3>What is OnKun?</h3>
                 <p>OnKun is a dictionary for Japanese kanji characters. Kanji are the Chinese characters that are used in Japanese writing alongside hiragana and katakana.</p>
                 <p>OnKun uses kanjiapi.dev, a fantastic api that provides kanji data for over 13,000 kanji.</p>
-                <p><a className={styles.link} href="https://www.kanjiapi.dev">Click here to find out more about kanjiapi.dev</a></p>
+                <p><a className={styles.link} href="https://kanjiapi.dev/">Click here to find out more about kanjiapi.dev</a></p>
                 <br />
                 <h3>Why the name "OnKun"?</h3>
                 <p>Kanji characters can be tricky since there are usually at least two ways that each character can be read. These readings are split into "on-yomi" (based on how these characters would have sounded like in Chinese at the time they were adopted) and kun-yomi (based on the pronunciation of a native Japanese word which has a similar meaning to that character). </p>
